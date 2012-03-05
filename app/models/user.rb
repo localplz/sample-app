4ff75646b75f97738728015fda10ae40ace57897
Alex Gerber <agerber23@gmail.com>
2012-03-05 03:06:07 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  before_save :create_remember_token
+
   
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true
@@ -20,4 +22,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
